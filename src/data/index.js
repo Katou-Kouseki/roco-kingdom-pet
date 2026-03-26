fd71beb90f8218db2ddda99cf5c6f3bb048b1f7b
Leo Chan <149321384+HackerLion03@users.noreply.github.com>
2026-03-26 01:31:12 -0400

    Delete src directory

--- src/data/index.js
+++
@@ -1,9 +0,0 @@
-import { petList1 } from './pets01.js'
-import { petList2 } from './pets02.js'
-import { elementColors, allElements } from './elements.js'
-
-// 自动合并所有宠物列表
-export const petList = [...petList1, ...petList2]
-
-// 导出给页面使用
-export { elementColors, allElements }
